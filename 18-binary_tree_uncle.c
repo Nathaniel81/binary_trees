cfaeaf450536dde1deafd766d044e3f775b51748
Nathaniel81 <nathanield356@gmail.com>
2022-11-25 10:46:17 +0300

    15-18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * @brief 
+ * 
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+        return (NULL);
+	if (node->parent->parent->right == node->parent)
+		return (node->parent->parent->left);
+	return (node->parent->parent->right);
+}
